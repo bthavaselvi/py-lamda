af933571f3afe43e391ed1df8b1d47a2625eed18
bthavaselvi <ocr.force2024@gmail.com>
2024-03-10 10:13:02 -0600

    refactored code

--- ocr/lambda_function.py
+++ ocr/lambda_function.py
@@ -2,28 +2,22 @@ from flask import Flask, request, jsonify
 import awsgi
 from common.OCRServiceFactory import OCRServiceFactory
 
-from io import BytesIO
 import traceback
 
-import json
-import os
-import base64
-from common.DocumentType import DocumentType
 
-print('i ma here')
 app = Flask(__name__)
 
-# @app.route('/analyze', methods=['POST'])
-# def analyze_document():
-#     try:
-#         # Get the file from the request
-#         file = request.files['file']
-#         document_type = request.form.get('documentType')
-#         file_content = file.read()
-#         return jsonify(OCRServiceFactory.create_OCR_service(document_type).analyze_document(file_content)),200
-#     except Exception as e:
-#         return jsonify({'error': str(e)}), 500
-print('2')
+@app.route('/analyze', methods=['POST'])
+def analyze_document():
+    try:
+        # Get the file from the request
+        file = request.files['file']
+        document_type = request.form.get('documentType')
+        file_content = file.read()
+        return jsonify(OCRServiceFactory.create_OCR_service(document_type).analyze_document(file_content)),200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 @app.route('/sayHello', methods=['GET'])
 def sayHello():
     print('3')
