21ca2cd6791c3fcf951e222a1cc8d43ad783375c
bthavaselvi <ocr.force2024@gmail.com>
2024-03-10 12:50:06 -0600

    json

--- ocr/response/BusinessCard.py
+++ ocr/response/BusinessCard.py
@@ -1,11 +1,15 @@
-class BusinessCard:
+import json
+class BusinessCard:  
     def __init__(self,firstName,lastName,emailId,address,phone):
         self.firstName = firstName
         self.lastName = lastName
         self.emailId = emailId
         self.address = address
         self.phone = phone
-        
+
+    def toJson(self):
+       return json.dumps(self, default=lambda o: o.__dict__)
+
     # class Address:
     #     def __init__(self,street,city,state,zipcode)
     #         self.street = street
