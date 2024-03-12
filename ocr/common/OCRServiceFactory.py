0d48f1c99e728f767955e3ff875a49ceef171065
bthavaselvi <ocr.force2024@gmail.com>
2024-03-12 00:22:53 -0600

    enum value

--- ocr/common/OCRServiceFactory.py
+++ ocr/common/OCRServiceFactory.py
@@ -4,9 +4,9 @@ from service.OCRService import BusinessCardService,InvoiceService
 class OCRServiceFactory:
     @staticmethod 
     def create_OCR_service(document_type):
-        if document_type == DocumentType.BUSINESS_CARD.name :
+        if document_type == DocumentType.BUSINESS_CARD.value :
             return  BusinessCardService()
-        elif document_type == DocumentType.INVOICE.name:
+        elif document_type == DocumentType.INVOICE.value:
             return InvoiceService()
         else:
             raise Exception('Invalid document type')
