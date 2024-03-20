bf91fbd2e3f517ad8fb495ea61b8beffe70e87aa
bthavaselvi <ocr.force2024@gmail.com>
2024-03-19 21:39:08 -0600

    expenseDocument entity

---
+++ ocr/response/ExpenseDocument.py
@@ -0,0 +1,46 @@
+from decimal import Decimal
+class Address:
+    def __init__(self,name:str , street:str, city:str,state:str,zip_code:str):
+        self.name =  name
+        self.street = street
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+class ReceiverBillTo:
+    def __init__(self,address: Address):
+        self.address = address
+
+class ReceverShipTo:
+    def __init__(self,address:Address):
+        self.address = address
+
+class Vendor:
+    def __init__(self,address:Address):
+        self.address = address
+
+class LineItem:
+    def __init__(self,expenseRowNumber:int,item:str,quantity: Decimal,unitPrice:Decimal,price: Decimal,
+                 productCode:str):
+        self.expenseRowNumber = expenseRowNumber
+        self.item = item
+        self.quantity = quantity
+        self.unitPrice = unitPrice
+        self.price = price
+        self.productCode = productCode
+
+class ExpenseDocument:
+    def __init__(self,invoiceReceiptDate:str,invoiceReceiptId:str,poNumber:str,
+                 paymentTerm:str, subTotal:Decimal, tax: Decimal,
+                 total:Decimal,lineItems : list[LineItem],receiverBillTo:ReceiverBillTo,
+                 receiverShipTo:ReceverShipTo,vendor:Vendor):
+        self.lineItems = lineItems
+        self.invoiceReceiptDate = invoiceReceiptDate
+        self.invoiceReceiptId = invoiceReceiptId
+        self.poNumber = poNumber
+        self.paymentTerm = paymentTerm
+        self.subTotal = subTotal
+        self.tax = tax
+        self.total = total
+        self.lineItems = lineItems
+        
+        
--- ocr/service/OCRService.py
+++ ocr/service/OCRService.py
@@ -7,6 +7,7 @@ from response.BusinessCard import BusinessCard
 import traceback
 import logging
 import boto3
+from response.ExpenseDocument import Address,ReceiverBillTo,ReceverShipTo,Vendor,LineItem,ExpenseDocument
 
 log = logging.getLogger("my-logger")
 
@@ -69,10 +70,50 @@ class BusinessCardService(OCR):
              raise
 
 class InvoiceService(OCR):
+
+    def toExpenseDocument(self,expenseDocument):
+       field_group =  expenseDocument.summary_groups
+       receiver_bill_to = field_group['RECEIVER_BILL_TO']
+       receiver_bill_address = Address(receiver_bill_to['NAME'],receiver_bill_to['STREET'],
+                                       receiver_bill_to['CITY'],receiver_bill_to['STATE'],
+                                       receiver_bill_to['ZIP_CODE'] )
+       
+       receiver_ship_to = field_group['RECEIVER_SHIP_TO']
+       receiver_ship_address = Address(receiver_ship_to['NAME'],receiver_ship_to['STREET'],
+                                       receiver_ship_to['CITY'],receiver_ship_to['STATE'],
+                                       receiver_ship_to['ZIP_CODE'] )
+       
+       vendor = field_group['VENDOR']
+       vendor_address =  Address(vendor['NAME'],vendor['STREET'],
+                                       vendor['CITY'],vendor['STATE'],
+                                       vendor['ZIP_CODE'] )
+       
+       line_items  = []
+       for line_item in expenseDocument.line_items_groups:
+          line = LineItem(line_item['EXPENSE_ROW'],line_item['ITEM'],
+                    line_item['QUANTITY'],line_item['UNIT_PRICE'],line_item['PRICE'],
+                    line_item['PRODUCT_CODE'])
+          line_item.append(line)
+
+       summary_fields  = expenseDocument.summary_fields
+
+       return ExpenseDocument(summary_fields['INVOICE_RECEIPT_DATE'],summary_fields['INVOICE_RECEIPT_ID'],
+                       summary_fields['PO_NUMBER'],summary_fields['PAYMENT_TERMS'],
+                       summary_fields['SUBTOTAL'],summary_fields['TAX'],summary_fields['TOTAL'],
+                       line_items,receiverBillTo = ReceiverBillTo(receiver_bill_address),
+                       receiverShipTo=ReceverShipTo(receiver_ship_address),vendor=Vendor(vendor_address))
+       
+           
+
     def analyze_document(self, data: bytes):
          try:
-            response =  textract_client.analyze_expense(Document={'Bytes': data})          
-            return response_parser.parser_analyze_expense_response(response)
+            response =  textract_client.analyze_expense(Document={'Bytes': data})   
+            expense_document = response_parser.parser_analyze_expense_response(response).expense_documents[0]       
+            expense_doc_to_return =  toExpenseDocument(expense_document)
+            return expense_doc_to_return
          except Exception as e:
              traceback.print_exc()
              raise
+         
+
+        
