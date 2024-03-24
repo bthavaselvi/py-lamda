e84c2a2863d9da94513f2372cfb6ba82f86c766b
bthavaselvi <ocr.force2024@gmail.com>
2024-03-23 20:42:09 -0600

    revert back

--- ocr/service/OCRService.py
+++ ocr/service/OCRService.py
@@ -111,10 +111,10 @@ class InvoiceService(OCR):
     def analyze_document(self, data: bytes):
         
             response =  textract_client.analyze_expense(Document={'Bytes': data})   
-            # expense_document = response_parser.parser_analyze_expense_response(response).expense_documents[0]       
-            # expense_doc_to_return =  self.toExpenseDocument(expense_document)
-            # return expense_doc_to_return
-            return json.load(response)
+            expense_document = response_parser.parser_analyze_expense_response(response).expense_documents[0]       
+            expense_doc_to_return =  self.toExpenseDocument(expense_document)
+            return expense_doc_to_return
+            
       
 class IDService(OCR):
     
