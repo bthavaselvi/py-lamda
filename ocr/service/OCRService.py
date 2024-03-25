f6b6fbfa8a8bc65dc14e0e36b9edf67860d729bd
bthavaselvi <ocr.force2024@gmail.com>
2024-03-24 19:15:03 -0600

    expense

--- ocr/service/OCRService.py
+++ ocr/service/OCRService.py
@@ -157,7 +157,7 @@ class InvoiceService(OCR):
        print(expenseDocument.line_items_groups)
        print(type( expenseDocument.line_items_groups))
        for line_item in expenseDocument.line_items_groups:
-           line_item.append(self,fromExpense(line_item.expenses))
+           line_items.append(self,fromExpense(line_item.expenses))
 
        summary_fields  = toSummaryFields(self,expenseDocument.summary_fields)
 
