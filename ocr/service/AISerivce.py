37dc0808a35f8af02835f228cc6f0f64246ee827
bthavaselvi <ocr.force2024@gmail.com>
2024-05-12 21:25:43 -0600

    client.chat.completions.create

--- ocr/service/AISerivce.py
+++ ocr/service/AISerivce.py
@@ -22,7 +22,7 @@ class AIService:
             "Taxes": ["Income taxes","Sales taxes","Property taxes"]
         # Define keywords for other categories similarly
         }
-         response = self.client.ChatCompletions.create(
+         response = self.client.chat.completions.create(
                         model="gpt-3.5-turbo",  
                         messages=[
                             {
